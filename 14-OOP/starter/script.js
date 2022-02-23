6220457b75a6bbbc14f77151b0c4e80334461de3
Sourabh Semalty <42134981+Sourabh-Semalty@users.noreply.github.com>
2022-02-23 07:41:43 +0530

    Learn about the concept of the prototype in the js

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const calcBirth = (birthYear) => {
-    return new Date().getFullYear() - birthYear;
-};
 const Person = function (fname, lname, birthYear) {
     // Instance Properties
     this.fname = fname;
@@ -10,6 +7,10 @@ const Person = function (fname, lname, birthYear) {
     this.birthYear = birthYear;
 };
 
-
 const sourabh = new Person('Sourabh', 'Semalty', 1998);
-console.log(calcBirth(sourabh.birthYear));
+
+// Prototypes
+Person.prototype.calcAge = function () {
+    return new Date().getFullYear() - this.birthYear;
+};
+console.log(sourabh.calcAge());

0f67463089a3289688e151408f5b7a551a865bc7
Sourabh Semalty <42134981+Sourabh-Semalty@users.noreply.github.com>
2022-02-23 07:29:48 +0530

    Object oriented concept started 1. Learn Constructor

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const calcBirth = (birthYear) => {
+    return new Date().getFullYear() - birthYear;
+};
+const Person = function (fname, lname, birthYear) {
+    // Instance Properties
+    this.fname = fname;
+    this.lname = lname;
+    this.birthYear = birthYear;
+};
+
+
+const sourabh = new Person('Sourabh', 'Semalty', 1998);
+console.log(calcBirth(sourabh.birthYear));
